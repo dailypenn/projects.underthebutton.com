c2679a7e3c88d919502ec4d6fd738dff9652ae68
Eric Zhang <EricZhang@ConAdmins-MacBook-Pro.local>
2018-10-21 18:22:41 -0400

    up and down votes

--- 2018/08/penn-dictionary/script.js
+++ 2018/08/penn-dictionary/script.js
@@ -1,47 +1,245 @@
+var definitions = {
+  'amy-gutmann': {
+    word: "Amy Gutmann",
+    definition: "The soul of the eighth President of the University of Pennsylvania, a <a href='https://www.underthebutton.com/article/2017/03/amy-gutmann-weird-video-op-ed-found' target='_blank'>disembodied spirit imagined</a>, usually as a vague, shadowy or evanescent form, as wandering among campus during her free time.",
+    upvotes: 0,
+    downvotes: 0
+  },
+  'ben-franlkin': {
+    word: "Ben Franklin",
+    definition: "Founder of Penn. Slaveholder. <a href='https://www.underthebutton.com/article/2017/03/benjamin-franklin-daylight-saving-time-petition-protest' target='_blank'>Scientist</a>. Womanizer. We’re pretty sure he’s a good swimmer. He’s dead now.",
+    upvotes: 0,
+    downvotes: 0
+  },
+  "ben-on-the-bench": {
+    word: "Ben on the Bench",
+    definition: "We’re not sure people pee on it as much as the legends say, but you can’t help but smile when you see a family huddle around the bench for a picture.",
+    upvotes: 0,
+    downvotes: 0
+  } 
+//   {
+//     word: "BioPond",
+//     definition: "Turtles, weed, and maybe some sort of science happens here. We’re pretty sure it’s <a href='https://www.underthebutton.com/article/2017/01/girl-scouts-bio-pond' target='_blank'>mostly just weed</a>, though.",
+//     upvotes: 0,
+//     downvotes: 0
+//   },
+//   {
+//     word: "Bursar",
+//     definition: "The most socially acceptable way to be an asshole to your parents.",
+//     upvotes: 0,
+//     downvotes: 0
+//   },
+//   {
+//     word: "Canada Goose",
+//     definition: "(Branta canadensis maxima) <a href='https://www.underthebutton.com/article/2017/03/canada-goose-ipo-parents-have-to-own-shares' target='_blank'>Species of wild goose</a> native to the arctic regions of North America. Can be identified by its black head and neck, brown body with white patches, and always-clean Stan Smiths.",
+//     upvotes: 0,
+//     downvotes: 0
+//   },
+//   {
+//     word: "Consulting",
+//     definition: "You’re not sure what it is, but <a href='https://www.underthebutton.com/article/2017/02/junior-returns-to-unfulfilling-internship' target='_blank'>you really want to &ldquo;go into&rdquo; it</a> when you graduate.",
+//     upvotes: 0,
+//     downvotes: 0
+//   },
+//   {
+//     word: "DFMO",
+//     definition: "Dance Floor Make Out. You hate seeing people do it until you do it, without realizing that you’re as obnoxious about it as everyone else. We get it, you have a mouth.",
+//     upvotes: 0,
+//     downvotes: 0
+//   },
+//   {
+//     word: "DRL",
+//     definition: "Die, Reincarnate, Leave.",
+//     upvotes: 0,
+//     downvotes: 0
+//   },
+//   {
+//     word: "Franzia",
+//     definition: "“Delicate pink hue with strawberry flavors, <a href='http://www.34st.com/article/2017/02/franzia-horoscopes' target='_blank'>easy to drink</a> and very refreshing. Perfect with lighter foods. Serve chilled.”",
+//     upvotes: 0,
+//     downvotes: 0
+//   },
+//   {
+//     word: "Frontera",
+//     definition: "Sceney and cheesy.",
+//     upvotes: 0,
+//     downvotes: 0
+//   },
+//   {
+//     word: "High Rise Wind Tunnel",
+//     definition: "High Rise Wind Tunnel (unofficially referred to as Hurricane Sandy (unofficially referred to as &ldquo;Superstorm Sandy&rdquo;) was the deadliest and most destructive hurricane of the 2012 Atlantic hurricane season, and the second-costliest hurricane in United States history.",
+//     upvotes: 0,
+//     downvotes: 0
+//   },
+//   {
+//     word: "Hoodie Allen",
+//     definition: "Penn’s <a href='https://www.underthebutton.com/article/2016/12/hoodie-allen-petition' target='_blank'>worst musical act</a>.",
+//     upvotes: 0,
+//     downvotes: 0
+//   },
+//   {
+//     word: "Huntsman",
+//     definition: "The <a href='https://www.underthebutton.com/article/2017/03/huntsman-russian-gsrs' target='_blank'>best place on campus</a> to catch a glimpse of the enormous size of Penn’s endowment.",
+//     upvotes: 0,
+//     downvotes: 0
+//   },
+//   {
+//     word: "Leaf Blower",
+//     definition: "To “leaf blow” someone is to shut them up, <a href='https://www.underthebutton.com/article/2017/01/top-5-leaf-blowers-to-drown-out-hate-filled-protests' target='_blank'>like the University did to the preacher on campus</a>. Ex: Preacher: homosexuality is a sin. University: check out this leaf blower, pretty loud huh?",
+//     upvotes: 0,
+//     downvotes: 0
+//   },
+//   {
+//     word: "Locust Walk",
+//     definition: "Where you <a href='https://www.underthebutton.com/article/2017/02/penn-honors-failed-alumni-by-inscribing-their-names-in-loose-bricks-on-locust' target='_blank'>avoid eye contact</a> with the guy in your math recitation trying to get people to come to his a cappella show.",
+//     upvotes: 0,
+//     downvotes: 0
+//   },
+//   {
+//     word: "Marble MacBook covers",
+//     definition: "Your MacBook was designed carefully with a modern, sleek look in mind. Throw that out the window with computer covers designed to look like slabs of stone.",
+//     upvotes: 0,
+//     downvotes: 0
+//   },
+//   {
+//     word: "MERT",
+//     definition: "n. Penn's student-run medical emergency response team. v. <a href='https://www.underthebutton.com/article/2016/10/wanna-avoid-getting-merted-dont-go-to-gregory' target='_blank'>sending your fucked up friend to the hospital</a> to get their stomach pumped.",
+//     upvotes: 0,
+//     downvotes: 0
+//   },
+//   {
+//     word: "New College House",
+//     definition: "Proof that Penn can do good things for its students, but chooses not to.",
+//     upvotes: 0,
+//     downvotes: 0
+//   },
+//   {
+//     word: "NSO",
+//     definition: "New Student Orientation. Example: Person one: Hey, NSO was fun right? Person two: Yes. Person one: Haha, it’s too bad we have nothing else in common and have no real conversational skills to advance our friendship.",
+//     upvotes: 0,
+//     downvotes: 0
+//   },
+//   {
+//     word: "OZ",
+//     definition: "The <a href='https://www.underthebutton.com/article/2016/12/oz-releases-holiday-playlist' target='_blank'>reason why</a> Ashton Kutcher’s daughter will not be attending Penn.",
+//     upvotes: 0,
+//     downvotes: 0
+//   },
+//   {
+//     word: "Penn InTouch",
+//     definition: "<a href='https://www.underthebutton.com/article/2017/02/penn-intouch-condoms' target='_blank'>Condom design</a>, apparently.",
+//     upvotes: 0,
+//     downvotes: 0
+//   },
+//   {
+//     word: "Princeton",
+//     definition: "Penn’s <a href='https://www.underthebutton.com/article/2017/03/breaking-amy-gutmann-refuses-to-shake-princeton-presidents-hand-all-a-big-misunderstanding' target='_blank'>rival</a> - it’s pretty one sided though, as no one at Princeton goes around with “Puck Fenn” shirts. We work hard, and play hard. They work hard, and read harder.",
+//     upvotes: 0,
+//     downvotes: 0
+//   },
+//   {
+//     word: "Quaker Days",
+//     definition: "Basically that day where all the middle schoolers get to go to the high school and walk around in a mixture of awe and terror.",
+//     upvotes: 0,
+//     downvotes: 0
+//   },
+//   {
+//     word: "SABS",
+//     definition: "See And Be Seen. Prime spots to do this: tables outside Frontera, bench in front of Castle, suspended from a small crane on College Green in an all-denim outfit.",
+//     upvotes: 0,
+//     downvotes: 0
+//   },
+//   {
+//     word: "Second Round Interview",
+//     definition: "Similar to the first round interview, except you’re expected to cut your hair shorter than the first interview. This shows dedication. For the third round, even shorter.",
+//     upvotes: 0,
+//     downvotes: 0
+//   },
+//   {
+//     word: "Shabbatones",
+//     definition: "Penn’s <a href='https://www.underthebutton.com/article/2016/12/shabbatones-review' target='_blank'>greatest musical act</a>.",
+//     upvotes: 0,
+//     downvotes: 0
+//   },
+//   {
+//     word: "Sprint Football",
+//     definition: "Really easy to make fun of, but they could probably still beat you up.",
+//     upvotes: 0,
+//     downvotes: 0
+//   },
+//   {
+//     word: "Squirrels",
+//     definition: "They know your schedule. They know where you sleep. Sometimes they find big-ass food and that’s cool. They are not afraid of you. Also, <a href='https://www.underthebutton.com/article/2017/03/official-unofficial-penn-squirrel-catching-club-goes-public-meme-economists-weigh-in' target='_blank'>memes</a>.",
+//     upvotes: 0,
+//     downvotes: 0
+//   },
+//   {
+//     word: "Starbucks Under Commons",
+//     definition: "Low on outlets, high on prices.",
+//     upvotes: 0,
+//     downvotes: 0
+//   },
+//   {
+//     word: "The Tampons",
+//     definition: "A <a href='https://www.underthebutton.com/article/2016/12/penn-cant-afford-tampons-trump' target='_blank'>tampon</a> is a mass of absorbent material, primarily used as a feminine hygiene product. Historically, the word &ldquo;tampon&rdquo; originated from the medieval French word &ldquo;tampion&rdquo;, meaning a piece of cloth to stop a hole, a stamp, plug, or stopper. At present, tampons are designed to be easily inserted into the vagina during menstruation and absorb the menstrual flow. (From Wikipedia)",
+//     upvotes: 0,
+//     downvotes: 0
+//   },
+//   {
+//     word: "Under the Button",
+//     definition: "The <a href='https://www.underthebutton.com/article' target='_blank'>Greatest Publication on Campus</a>.",
+//     upvotes: 0,
+//     downvotes: 0
+//   },
+//   {
+//     word: "UPenn",
+//     definition: "What you call Penn when you want to stress that you don’t go to Penn State.",
+//     upvotes: 0,
+//     downvotes: 0
+//   },
+//   {
+//     word: "Van Pelt Library",
+//     definition: "The building with the <a href='https://www.underthebutton.com/article/2017/02/van-pelt-bag-check' target='_blank'>incredibly tight security</a> next to where you chain smoke.",
+//     upvotes: 0,
+//     downvotes: 0
+//   },
+//   {
+//     word: "Wawa",
+//     definition: "On-campus bakery that provides freshly baked bread for students at 3 a.m.",
+//     upvotes: 0,
+//     downvotes: 0
+//   },
+//   {
+//     word: "Writing Sem",
+//     definition: "University-sanctioned pain.",
+//     upvotes: 0,
+//     downvotes: 0
+//   }
+}
+
 var CSV_ENTRIES;
 
 const renderEntries = () => {
   entries = $('#entries');
 
-  for (var i in CSV_ENTRIES) {
-    if (i == 5) {
-      entries.append(
-        '<div class="row"><center class="col-sm-6">' +
-        '<div id="div-gpt-ad-1490568038751-4" style="height:250px; width:300px;">' +
-        '</div>' +
-        '</center><center class="col-sm-6">' +
-        '<div id="div-gpt-ad-1490568038751-0" style="height:250px; width:300px;"></div>' +
-        '</center></div><br>'
-      );
-    };
-
-    var entry = CSV_ENTRIES[i];
-    if (entry[0] !== "") {
-      entries.append(
-      '<div class="rectangle">' +
-        '<div class="content">' +
-          '<h1>' + entry[0] + '</h1>' +
-          entry[1] +
-        '</div>' +
-        '<div class = "vote">' +
-          '<div class = "counter">0</div>' +
-        '</div>' +
-      '</div>'+
-      '<br>'
-      );
-    }
+  for (var i in definitions) {
+    const definition = definitions[i];
+    entries.append(
+    '<div class="rectangle" id="' + i + '">' +
+      '<div class="content">' +
+        '<h1>' + definition.word + '</h1>' +
+        definition.definition +
+      '</div>' +
+      '<div class="vote">' +
+        '<div class="counter">0</div>' +
+        '<button class="btn btn-primary up">Up</button>' +
+        '<button class="btn btn-primary down">Down</button>' +
+      '</div>' +
+    '</div>'+
+    '<br>'
+    );
   }
-
-    var up = $('<button>').addClass('btn btn-primary up').text('Up');
-    var down = $('<button>').addClass('btn btn-danger down').text('Down');
-    $('.vote').append(up);
-    $('.vote').append(down);
-
-    $('.up').click(() => {
-      console.log(this);
-    })
 }
-
 
 
 function count() {
@@ -50,9 +248,13 @@ function count() {
 
 $(document).ready(() => {
   $.get('./penndictentries.csv', data => {
-    CSV_ENTRIES = $.csv.toArrays(data);
     renderEntries();
+
+    const ups = Array.from(document.getElementsByClassName('up'));
+    ups.forEach(up => {
+      up.addEventListener('click', () => {
+        console.log(up.parentElement.parentElement.id);
+      });
+    });
   });
-
-
 });
